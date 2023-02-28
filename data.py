4d7add9796eedd9c32f34a452ed56fb1af5d0f6b
maruf1361 <mp19@williams.edu>
2023-02-28 10:02:51 -0500

    Commit 1

---
+++ data.py
@@ -0,0 +1,41 @@
+question_data = [
+        {
+         "question": "The main character in the &quot;Half-Life&quot; franchise is named Morgan Freeman.",
+         "correct_answer": "False"
+        },
+        {
+         "question": "Leonardo DiCaprio won an Oscar for Best Actor in 2004&#039;s &quot;The Aviator&quot;.",
+         "correct_answer": "False"
+        },
+        {
+         "question": "No Game No Life first aired in 2014.",
+         "correct_answer": "True"
+        },
+        {
+         "question": "California is larger than Japan.",
+         "correct_answer": "True"
+        },
+        {
+         "question": "According to Greek Mythology, Atlas was an Olympian God.",
+         "correct_answer": "False",
+        },
+        {
+         "question": "&quot;Metal Gear Solid 3: Snake Eater&quot; was released in 2004.",
+         "correct_answer": "True"
+        },
+        {
+         "question": "Tupac Shakur died due to complications from being stabbed in 1996.", "correct_answer": "False",
+         },
+        {
+        "question": "Celiac Disease is a disease that effects the heart, causing those effected to be unable to eat meat.",
+        "correct_answer": "False"
+        },
+        {
+         "question": "The United States of America was the first country to launch a man into space.",
+         "correct_answer": "False"
+        },
+        {
+         "question": "Lead Singer Rivers Cuomo of American rock band Weezer attended Harvard.",
+         "correct_answer": "True"
+        }
+]
